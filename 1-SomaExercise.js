1c5384dcdfb8a35ac7b2f9c287dba246401d0c26
“MikhelCB” <srrynow@hotmail.com>
2025-06-02 19:24:46 -0300

    resolvendo exercises basicos

---
+++ 1-SomaExercise.js
@@ -0,0 +1,14 @@
+// 1. Soma de dois números
+// Crie um programa que peça dois números ao usuário e mostre a soma deles.
+
+// js
+// Copiar
+// Editar
+
+let A = 23;
+
+let B = 23;
+
+let Soma = A + B;
+
+console.log(Soma);
---
+++ 3.Contar1a10.js
@@ -0,0 +1,6 @@
+// 3. Contar de 1 até 10
+// Use um laço para imprimir os números de 1 a 10.
+
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
