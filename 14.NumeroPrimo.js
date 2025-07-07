af76f4b207fa19f32cfb92da2c5de447cdf6c418
Mikhel Cabral Barbosa <mikhel.barbosa@D1064.digitro.corp>
2025-07-07 11:52:11 -0300

    feat: exericio numero primo concluido

--- 14.NumeroPrimo.js
+++ 14.NumeroPrimo.js
@@ -1,17 +1,23 @@
 //Verificar se um número é primo
 //Crie uma função que receba um número e retorne se ele é primo ou não.
+// Ele primeiro descarta o que ja nao e primo de certeza, ou seja, se o n for menor = 1, ja nao e numro primo
+// Agora é feito um for, que pega o i = 2 e vai listar até o n = 9 por exemplo, feito isso
+// é feito um if, que se o n for divisivel por algum numero até o 9 e o resto da divisao for 0, significa que ele não é so divisivel por ele mesmo ou por 1, logo ele não é primo
 
-function VerificaNumero(numero) {
-  if (numero ) {
-    console.log(numero);
-  } else {
-    console.log("Não é primo");
+function Ehprimo(n){
+  if(n <= 1){
+    return false
   }
-}
-console.log(VerificaNumero(4));
 
-// 10. Criar função que retorne o dobro de um número
-// function dobrar(numero) {
-//     return numero * 2;
-//   }
-//   console.log(dobrar(4));
+  for(i = 2; i < n; i++){
+    if(n % i === 0){
+      return false
+    }
+  }
+  return true
+}
+
+
+
+
+console.log(Ehprimo(4));  // true
