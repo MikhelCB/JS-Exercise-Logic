5d1f4ada20cd509dc92c35e7dc37f1a6a75d3710
Mikhelcb <srrynow@hotmail.com>
2025-07-07 19:35:59 -0300

    feat: finished logic for function factorial number

--- 15.Fatorial.js
+++ 15.Fatorial.js
@@ -1,28 +1,16 @@
 //5. Calcular o fatorial de um número
 //Crie uma função que receba um número e retorne seu fatorial.
 
-// let total = 5;
 
+function fatorial (n){
+    let total = 1;
+    for (i = n; i >= 1; i--) {
+        total *= i;
+    }
 
-// function fatorial(n){
-
-//   for(i = 1; i <= n; i++){
-//     total *= i
-//   }
-// }
-// console.log(fatorial(2))
-
-
-
-
-// function fatorial(n){
-    
-// }
-// console.log(fatorial(3))
-
-
-let total = 0;
-
-for (i = 10; i >= 1; i--) {
-    console.log(total)
+    return total
 }
+
+console.log(fatorial(4))
+
+
