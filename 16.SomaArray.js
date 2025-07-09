147b38a07c79fd5e0c4e9b0ff972be4c6b212cb9
Mikhelcb <srrynow@hotmail.com>
2025-07-08 22:18:47 -0300

    Commented exercise for study

--- 16.SomaArray.js
+++ 16.SomaArray.js
@@ -1,12 +1,20 @@
 // 🔹 6. Criar função que recebe um array de números e retorna a média
 // Exemplo: [10, 5, 8] → média = 7.6
-let numeros = [6, 36, 3];
-let acumuladora = 0;
+
+// O lenght é uma propriedade no javascript que diz quantos elementos tem no array ########
+//Eu crio uma função chamada mediaArray e passo como parâmetro o array com os numeros
+// Eu crio uma variável acumuladora e crio um laço for, que é um contador, ele vai contar até o número de elementos que estiver dentro do array(numeros)
+//Em cada volta do for, é somado o numero atual do array, acumuladora += array[i] || ex: i = 0 → array[0] = 6 → acumuladora = 6 || i = 1 → array[1] = 36 → acumuladora = 6 + 36 = 42
 
 function mediaArray(array) {
-  for (let i = 0; i < numeros.length; i++) {
+  let acumuladora = 0;
+
+  for (let i = 0; i < array.length; i++) {
     acumuladora += array[i];
   }
-  acumuladora / array.length;
+  return acumuladora / array.length;
 }
-console.log(acumuladora);
+
+let numeros = [6, 36, 3, 4];
+let resultado = mediaArray(numeros);
+console.log("Média:", resultado); // 15
---
+++ 17.Contarvogais.js
@@ -0,0 +1,2 @@
+// 7. Função que recebe uma palavra e retorna quantas vogais ela tem
+// Dica: use .includes() e percorra letra por letra com for.
