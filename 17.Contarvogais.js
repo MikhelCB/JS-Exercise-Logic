9842e44cf1cd44215229f9707cfc53a18a298e31
“MikhelCB” <srrynow@hotmail.com>
2025-07-09 18:58:57 -0300

    exercise completed

--- 17.Contarvogais.js
+++ 17.Contarvogais.js
@@ -1,34 +1,18 @@
 // 7. Função que recebe uma palavra e retorna quantas vogais ela tem
 // Dica: use .includes() e percorra letra por letra com for.
 
-let palavra = "aaaaaaa";
-let vogais = "a, e , i , o , u";
-
+let palavra = "Mikhel";
+let vogais = "aeiou";
 let contador = 0;
 
 for (let i = 0; i < palavra.length; i++) {
-  contador++;
+  let letra = palavra[i].toLowerCase();
 
-  console.log(contador);
+  console.log(palavra[i]);
+
+  if (vogais.includes(letra)) {
+    contador++;
+  }
 }
 
-// 🔹 6. Criar função que recebe um array de números e retorna a média
-// Exemplo: [10, 5, 8] → média = 7.6
-
-// O lenght é uma propriedade no javascript que diz quantos elementos tem no array ########
-// Eu crio uma função chamada mediaArray e passo como parâmetro o array com os numeros
-// Eu crio uma variável acumuladora e crio um laço for, que é um contador, ele vai contar até o número de elementos que estiver dentro do array(numeros)
-// Em cada volta do for, é somado o numero atual do array, acumuladora += array[i] || ex: i = 0 → array[0] = 6 → acumuladora = 6 || i = 1 → array[1] = 36 → acumuladora = 6 + 36 = 42
-
-// function mediaArray(array) {
-//   let acumuladora = 0;
-
-//   for (let i = 0; i < array.length; i++) {
-//     acumuladora += array[i];
-//   }
-//   return acumuladora / array.length;
-// }
-
-// let numeros = [6, 36, 3, 4];
-// let resultado = mediaArray(numeros);
-// console.log("Média:", resultado); // 15
+console.log("Quantidade de vogais:", contador);
